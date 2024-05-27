803b3350d8cb8693a9cb1350cd776f3b4740e64e
drfiffi <drfiffi123@gmail.com>
2024-05-27 16:32:48 -0400

    SONICHU FEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET
    i love you tails feet :heart_eyes:

---
+++ data/events/Events.hx
@@ -0,0 +1,18 @@
+function onEvent(_){
+    if (_.event.name == "Events") {
+        var types = switch(_.event.params[0]){
+            case 'Foot Kisser':
+                foot = new FlxSprite(0, 0).loadGraphic(Paths.image('stages/sonichu/footsy1'));
+                foot.cameras = [camHUD];
+                foot.scale.set(0.7, 0.7);
+	            add(foot);
+
+                FlxTween.tween(foot, {alpha: 0}, 1.2, {ease: FlxEase.linear});
+                FlxTween.tween(foot.scale, {x: 0.3, y: 0.3}, 1, {ease: FlxEase.quadOut});
+        }
+    }
+}
+
+function update(){
+    foot.screenCenter();
+}
