e23cab136085a75d4c6fcc81b255bb1f194f4718
drfiffi <drfiffi123@gmail.com>
2024-07-08 10:49:01 -0400

    Events update
    Organizational stuff + Duet Hudson event

--- data/events/Events.hx
+++ data/events/Events.hx
@@ -1,18 +1,44 @@
+import haxe.io.Input;
+import funkin.backend.utils.NdllUtil;
+import sys.FileSystem;
+import Sys;
+
+import funkin.backend.utils.NativeAPI;
+
+var notifSend = NdllUtil.getFunction("notification", "notification_send", 3);
+var notifDelete = NdllUtil.getFunction("notification", "notification_delete", 0);
+function sendNotif(title:String, desc:String, taskIconPath:String) {
+    //the taskbar icon argument only accepts .ico files, sorry!!
+    var args:Array<String>;
+
+    #if windows //FOR WINDOWS
+    notifSend(title, desc, FileSystem.absolutePath(taskIconPath));
+    #elseif mac //FOR MAC
+    notifSend(title, desc, FileSystem.absolutePath(taskIconPath));
+    #elseif linux //FOR LINUX
+    args = ["-u", "normal", title, desc]
+    Sys.command("notify-send", args);
+    #end
+}
+
 function onEvent(_){
     if (_.event.name == "Events") {
         var types = switch(_.event.params[0]){
-            case 'Foot Kisser':
-                foot = new FlxSprite(0, 0).loadGraphic(Paths.image('stages/sonichu/footsy1'));
-                foot.cameras = [camHUD];
-                foot.scale.set(0.7, 0.7);
-	            add(foot);
-
-                FlxTween.tween(foot, {alpha: 0}, 1.2, {ease: FlxEase.linear});
-                FlxTween.tween(foot.scale, {x: 0.3, y: 0.3}, 1, {ease: FlxEase.quadOut});
+            case 'Default Cam Zoom (Global)':
+                defaultCamZoom = _.event.params[1];
+                trace('Zoom Set');
+            case 'Notification (Global)':
+                #if windows
+                    sendNotif(_.event.params[1], _.event.params[2], "icon.ico");
+                    trace('Notification Sent');
+                    window.onClose.add(function() {
+                    notifDelete();
+                });
+                #end
+            case 'Popup (Global)':
+                NativeAPI.showMessageBox(_.event.params[1], _.event.params[2], 'icon.ico');
+            case 'Window Name (Global)':
+                window.title = _.event.params[1];
         }
     }
-}
-
-function update(){
-    foot.screenCenter();
 }
